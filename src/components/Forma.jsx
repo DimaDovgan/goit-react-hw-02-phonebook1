db03adea0d4c5ace0bbb424038b60314aba56b9a
DimaDovgan <89789155+DimaDovgan@users.noreply.github.com>
2022-05-09 22:07:03 +0300

    bvbdg

--- src/components/Forma.jsx
+++ src/components/Forma.jsx
@@ -35,7 +35,7 @@ export class Form extends Component{
         type="text"
         name="name"
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        title="Name  may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
         value={name}
                 onChange={this.hendelChangeName} /></label>
@@ -45,7 +45,7 @@ export class Form extends Component{
         type="tel"
         name="number"
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        title="Phone number must be digits  and can contain spaces, dashes, parentheses and can start with +"
         required
         value={number}
         onChange={this.hendelChangeNumber}
